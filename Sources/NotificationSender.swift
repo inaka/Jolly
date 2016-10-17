4fd8eb9a3bb6c26b4d72df2a5f9a8041f121896f
Pablo Villar <pablo@inakanetworks.com>
2016-10-17 10:16:03 -0300

    Add license headers.

--- Sources/NotificationSender.swift
+++ Sources/NotificationSender.swift
@@ -1,10 +1,18 @@
+// NotificationSender.swift
+// Jolly
 //
-//  NotificationSender.swift
-//  TestRequest
+// Copyright 2016 Erlang Solutions, Ltd. - http://erlang-solutions.com/
 //
-//  Created by Pablo Villar on 10/7/16.
-//  Copyright © 2016 Inaka. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import Foundation
 
