302dd2f501fb850f69d745e2e8474877b1015692
Pablo Villar <pablo@inakanetworks.com>
2016-10-18 10:46:56 -0300

    Add RepoSpecParser tests.

---
+++ Tests/JollyTests/RepoSpecParserTests.swift
@@ -0,0 +1,27 @@
+import XCTest
+@testable import Jolly
+
+class RepoSpecParserTests: XCTestCase {
+    
+    func testParseSuccess() {
+        let dict: [String: Any] = ["html_url": "url",
+                                   "full_name": "inaka/jolly",
+                                   "stargazers_count": 1,
+                                   "forks": 2,
+                                   "open_issues_count": 3]
+        let spec = RepoSpecParser().repoSpec(from: dict)
+        XCTAssertNotNil(spec)
+        XCTAssertEqual(spec!.url.absoluteString, "url")
+        XCTAssertEqual(spec!.fullName, "inaka/jolly")
+        XCTAssertEqual(spec!.stars, 1)
+        XCTAssertEqual(spec!.forks, 2)
+        XCTAssertEqual(spec!.issues, 3)
+    }
+    
+    func testParseFailure() {
+        let dict = [String: Any]()
+        let spec = RepoSpecParser().repoSpec(from: dict)
+        XCTAssertNil(spec)
+    }
+    
+}
