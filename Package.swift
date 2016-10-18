b24b8196faab95bea7f72a6a1fba5d389cbc468c
Pablo Villar <pablo@inakanetworks.com>
2016-10-18 07:51:09 -0300

    Package.swift fix, remove duplicate dependency.

--- Package.swift
+++ Package.swift
@@ -4,6 +4,5 @@ let package = Package(
     name: "Jolly",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0)
     ])
